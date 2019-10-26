b398c5beb6da2f47ac259c0ee7bb2635f71c0138
indahkw <56398559+indahkw@users.noreply.github.com>
2019-10-26 11:43:34 +0700

    Add files via upload

---
+++ latihan 1.py
@@ -0,0 +1,11 @@
+#ini variabel & nilai
+
+namalengkap = "indah kurniawati"
+namapanggilan = "indah"
+npm = "3119110502"
+tempatlahir = "magetan"
+umur = "19"
+telf = "0896 6621 3339"
+alamat = "bekasi"
+
+print("assalamualaikum, \n let me introduce my self, My name is",namalengkap,",but you can call me",namapanggilan,". My NPM is",npm,". i was born in",tempatlahir,"and i am",umur,"yearsold. I am very glad if you want to invite my house in",alamat,". so, don't forgrt to call me before with the number",telf,"\n Thank you")
